e819668a03a5a701d71ac76b591a235eb019a5d4
yazer <yazeraz0509@gmail.com>
2022-01-28 12:56:32 +0530

    Added session manager

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -14,14 +14,20 @@ func WriteToConsole(next http.Handler) http.Handler {
 	})
 }
 
+// Add csrf token to all post request
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
-		Path: "/",
-		Secure: false,
+		Path:     "/",
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// Loads and saves session on everyrequest
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module golang-hotel-booking-system
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/alexedwards/scs v1.4.1
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
+)
 
-require github.com/justinas/nosurf v1.1.1 // indirect
+require (
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20220126234351-aa10faf2a1f8 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
